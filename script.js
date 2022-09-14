b96398f7188c92c7cf2ca42f4fd0ee6f710e41dc
Marcos Gabriel <96658017+Marcos-Gabriell@users.noreply.github.com>
2022-09-14 09:55:52 -0300

    Update script.js
    adicionando js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings:["Saúde publica", "na nossa região"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+});
